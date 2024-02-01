28c19656c3b599450f695aaa1008bd297a47bd8c
thanhyx4 <{ID}+{username}@users.noreply.github.com>
2024-02-02 01:12:44 +0700

    config Amplification

--- Amplificacion/inject_pcap.sh
+++ Amplificacion/inject_pcap.sh
@@ -3,17 +3,20 @@
 
 INPUT="./input/"    #dns-hdns-02_2024-01-07_00_01.pcap.gz
 OUTPUT="./output/"
-NUM_PACKET=10
+NUM_PACKET=10           #numbers packets per second
 INITIAL_TIME=0
-DURATION=60
+DURATION=60             #seconds
 ZOMBIES=1
-WINDOWS=0.01
-PPS=42
-SERVER="117.122.125.80"
+WINDOWS=0.01            #window size
+PPS=42                    #packets per window
+SERVER="117.122.125.80"     #server ip
+SourceP=21517
 
 #each time read file -> create different botnets -> consuming time
 #bash inject_pcap.sh "*.pcap"     #must have ""
-
+#required paremeters
+TARGET="8.8.8.8"
+DOMAIN="vnnic.vn"
 
 for filename in $1; do
     TMP="${OUTPUT}${filename:8:-3}"
@@ -23,6 +26,7 @@ for filename in $1; do
       continue
     fi
     #TMP=$OUTPUT
-	  COMMAND="python3 DNSMain.py -i ${filename} -o ${TMP} -z 5 -n 5 -it 0 -p 10 -d 120 -d 300 -s "203.119.73.80""
+	  COMMAND="python3 mainAmplification.py -i ${filename} -o ${TMP} -target ${TARGET} -dom ${DOMAIN} -z 5 -it 0  -d 120 -rtype $true -s "203.119.73.80""
+	  echo "${COMMAND}"
 	  sh -c "${COMMAND}"
 done
