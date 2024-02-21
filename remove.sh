0d36e1fede65b6b436b042d4e76cf7987120b8a4
thanhyx4 <{ID}+{username}@users.noreply.github.com>
2024-02-21 14:31:54 +0700

    rm files

--- remove.sh
+++ remove.sh
@@ -3,7 +3,7 @@
 #bash testPcap.sh "./Port\ Scanning/output/*.pcap" "./Port\ Scanning/output/"
 
 
-lst=("dnsFloodNXDOMAIN/input/*" "dnsFloodNXDOMAIN/output/*" "Amplificacion/input/*" "Amplificacion/output/*" "UDP\ Floods/input/*" "UDP\ Floods/output/*" "tcpSynFlood/input/*" "tcpSynFlood/output/*" "Port\ Scanning/input*" "Port\ Scanning/output*")
+lst=("dnsFloodNXDOMAIN/input/*" "dnsFloodNXDOMAIN/output/*" "Amplificacion/input/*" "Amplificacion/output/*" "UDP\ Floods/input/*" "UDP\ Floods/output/*" "tcpSynFlood/input/*" "tcpSynFlood/output/*" "Port\ Scanning/input/*" "Port\ Scanning/output/*")
 for i in "${lst[@]}"; do
   # shellcheck disable=SC2066
     COMMAND="rm -fr "$i""
